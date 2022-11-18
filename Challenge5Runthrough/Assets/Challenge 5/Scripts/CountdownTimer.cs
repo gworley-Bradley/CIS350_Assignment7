daa7bc38eb7fefa8cd146c6c2d2303e71f4bfc25
gworley-Bradley <gworley@mail.bradley.edu>
2022-11-18 06:12:38 -0600

    Added Challenge5Runthrough

---
+++ Challenge5Runthrough/Assets/Challenge 5/Scripts/CountdownTimer.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * (Gavin Worley)
+ * (Challenge 5)
+ * (Brief description of the code in the file.
+ *  Countdown timer for ending game)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountdownTimer : MonoBehaviour
+{
+    private GameManagerX gameManager;
+
+    public float timeLeft = 60.0f;
+    public Text startText; // used for showing countdown
+
+    public bool timerStarted = false;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+    }
+    void Update()
+    {
+        if (timerStarted)
+        {
+            timeLeft -= Time.deltaTime;
+            // Convert integer to string
+            startText.text = "Time Left: " + (timeLeft).ToString("0");
+            if (timeLeft < 1 || !gameManager.isGameActive)
+            {
+                startText.text = "Time Left: 0";
+                gameManager.GameOver();
+            }
+        }
+
+    }
+
+    public void StartTimer()
+    {
+        timerStarted = true;
+    }
+}
---
+++ Challenge5Runthrough/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * (Gavin Worley)
+ * (Challenge 5)
+ * (Brief description of the code in the file.
+ *  used to destroy the object particle after 2 seconds)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
