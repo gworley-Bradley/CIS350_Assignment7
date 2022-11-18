804b6794d4c9d747afdd4cff6085d73948e71e28
gworley-Bradley <gworley@mail.bradley.edu>
2022-11-18 05:32:30 -0600

    Added Prototype5Runthrough

---
+++ Prototype5Runthrough/Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    private int score;
+
+    public bool isGameActive;
+
+    public Button restartButton;
+
+    public GameObject titleScreen;
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            //wait 1 second
+            yield return new WaitForSeconds(spawnRate);
+
+            //pick a random index between 0 and the number of prefabs to spawn
+            int index = Random.Range(0, targets.Count);
+            //spawning the randomly selected prefab
+            Instantiate(targets[index]);
+
+            //testing UpdateScore by adding 5 everytime a new target spawns
+            // UpdateScore(5);
+        }
+    }
+
+    public void GameOver()
+    {
+
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        scoreText.text = "Score: " + score;
+
+    }
+}
---
+++ Prototype5Runthrough/Assets/Scripts/Target.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    public int pointValue;
+
+    private GameManager gameManager;
+
+    public ParticleSystem explosionParticle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        targetRb = GetComponent<Rigidbody>();
+        RandomForce();
+        RandomTorque();
+
+        //set the position with a randomized X value
+        transform.position = RandomSpawnPos();
+
+        //set reference to gameManager
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void RandomTorque()
+    {
+
+        //add a torque (rotational force) with randomized xyz values
+        targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), 
+            Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
+    }
+
+    private void RandomForce()
+    {
+        //add a force upwards multiplied by a randomized speed
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            gameManager.UpdateScore(pointValue);
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameObject.CompareTag("Bad"))
+        {
+             gameManager.GameOver();
+        }
+
+        Destroy(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
